553de1359471bcd8e52e2dbac7ff4c5a46d394cc
Carl Yan <94885426+Carl7yan@users.noreply.github.com>
2023-11-03 19:46:00 +0800

    Update timer_if.sv

--- tb/timer_if.sv
+++ tb/timer_if.sv
@@ -1,15 +1,18 @@
-interface apb_if(input pclk, presetn);
+interface apb_if(input pclk);
+  logic presetn;
   logic extin;
   logic timerint; 
 
   clocking drv_cb @(posedge pclk);
     default input #1 output #1;
+    output presetn;
     output extin;
     input  timerint;
   endclocking
 
   clocking mon_cb @(posedge pclk);
     default input #1;
+    input presetn;
     input extin;
     input timerint;
   endclocking
