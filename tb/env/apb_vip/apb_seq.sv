853cc2165fd248b00ea75e17cda34ec15b857be4
Carl Yan <94885426+Carl7yan@users.noreply.github.com>
2023-11-03 19:24:51 +0800

    Update apb_seq.sv

--- tb/env/apb_vip/apb_seq.sv
+++ tb/env/apb_vip/apb_seq.sv
@@ -24,6 +24,24 @@ class apb_rd_seq extends uvm_sequence #(apb_trans);
   extern virtual task body();
 endclass
 
+class apb_wr_random_seq extends uvm_sequence #(apb_trans);
+  `uvm_object_utils(apb_wr_random_seq)
+
+  apb_trans trans;
+  apb_cfg cfg;
+
+  rand bit [9:0] num;
+  constraint c1 {
+    soft num inside {[10:1000]};
+  }
+  
+  function new(string name = "apb_wr_random_seq");
+    super.new(name);
+  endfunction
+
+  extern virtual task body();
+endclass
+    
 task apb_wr_seq::body();
   trans = apb_trans::type_id::create("trans");
   cfg   = apb_cfg::type_id::create("cfg");
@@ -41,4 +59,18 @@ task apb_rd_seq::body();
   `uvm_do_with(trans, { trans.pwrite==0;
                         trans.paddr==cfg.paddr_cfg;
                       })
+endtask
+
+task apb_wr_random_seq::body();
+  trans = apb_trans::type_id::create("trans");
+  cfg   = apb_cfg::type_id::create("cfg");
+
+  for(int i = 0; i < num; i++) begin
+    `uvm_do_with(trans, { trans.pwrite==1;
+                          trans.paddr==cfg.paddr_cfg;
+                          trans.pwdata==cfg.pwdata_cfg;
+                        })
+    `uvm_info(get_name(),$sformatf("random num=%d, apb_trans.convert2str()=%s", num, apb_trans.convert2str()), UVM_HIGH)
+  end
+  `uvm_info(get_name(),$sformatf("random done"), UVM_LOW)
 endtask
