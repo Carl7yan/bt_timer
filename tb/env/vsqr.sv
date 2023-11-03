5f6c08f0b89320cd1e6d15e8d90b4b850bb94e59
Carl Yan <94885426+Carl7yan@users.noreply.github.com>
2023-11-03 14:15:18 +0800

    Update vsqr.sv

--- tb/env/vsqr.sv
+++ tb/env/vsqr.sv
@@ -12,8 +12,8 @@ endclass
 
   function void vsqr::end_of_elaboration_phase(uvm_phase phase);
     super.end_of_elaboration_phase(phase);
-    if(!uvm_config_db#(apb_sqr)::get(this, "", "apb_sqr", apb_sqr);
+    if(!uvm_config_db#(apb_sqr)::get(this, "", "apb_sqr", apb_sqr_v);
       `uvm_fatal(get_full_name(), "No apb_sqr specified in vsqr");
-    if(!uvm_config_db#(timer_sqr)::get(this, "", "timer_sqr", timer_sqr);
+    if(!uvm_config_db#(timer_sqr)::get(this, "", "timer_sqr", timer_sqr_v);
       `uvm_fatal(get_full_name(), "No timer_sqr specified in vsqr");    
   endfunction
